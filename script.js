9c903faeaafda17f3d6e0a4292aa0ab09652affd
Ruiz Sanchez <ruizcayetano@hotmail.com>
2020-06-26 20:19:04 -0600

    trying to fix my local storage

--- script.js
+++ script.js
@@ -2,40 +2,44 @@ var d = new Date();
 
 document.getElementById("currentDay").innerHTML = d.toDateString();
 
-$(document).ready(function () {
-    var appointments = new Array();
-    var appointment1 = {
-        id: "id1",
-        description: "George brings projector for presentations.",
-        location: "",
-        subject: "Quarterly Project Review Meeting",
-        calendar: "Room 1",
-        start: new Date(2019, 10, 23, 9, 0, 0),
-        end: new Date(2019, 10, 23, 16, 0, 0)
-    }
-    var appointment2 = {
-        id: "id2",
-        description: "",
-        location: "",
-        subject: "IT Group Mtg.",
-        calendar: "Room 2",
-        start: new Date(2019, 10, 24, 10, 0, 0),
-        end: new Date(2019, 10, 24, 15, 0, 0)
-    }
-    var appointment3 = {
-        id: "id3",
-        description: "",
-        location: "",
-        subject: "Course Social Media",
-        calendar: "Room 3",
-        start: new Date(2019, 10, 27, 11, 0, 0),
-        end: new Date(2019, 10, 27, 13, 0, 0)
-    }
-    var appointment4 = {
-        id: "id4",
-        description: "",
-        location: "",
-        subject: "New Projects Planning",
-        calendar: "Room 2",
-        start: new Date(2019, 10, 23, 16, 0, 0),
-        end: new Date(2019, 10, 23, 18, 0, 0)
+
+
+let entireWorkDay = {
+    "8 AM" : "",
+    "9 AM" : "",
+    "10 AM" : "",
+    "11 AM" : "",
+    "12 PM" : "",
+    "1 PM"  : "",
+    "2 PM" : "",
+    "3 PM" : "",
+    "4 PM" : "",
+    "5 PM" : "",
+};
+//ince I labeled several ids on each row numerically, setting this variable will let me more easily reference said ids.
+let counter = 1;
+//Initial function on page load. Sets local storage if there is none currently. Otherwise, it parses what is already there.
+$(document).ready(function() {
+    if (!localStorage.getItem('entireWorkDay')){
+        updateDailyTasks(entireWorkDay);
+    } else {
+        let object = JSON.parse(localStorage.getItem('entireWorkDay'))
+        updateDailyTasks(object)
+        }
+
+
+        var dateHeader = document.getElementById('currentDay');
+ 
+        var current = moment().format('MMMM Do YYYY,')
+
+         dateHeader.textContent = current;
+         console.log(dateHeader);
+
+});
+
+function updateDailyTasks (workdayObject){
+ $(".calender-row").each(function(){
+    let texting = $(this).children("div");
+    $(this).children("textarea").text(workdayObject[texting.text()])
+ })
+}
